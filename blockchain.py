f4015254e1d6f00f32dba8a49e0ab2e16418b154
konguele <juanjo.gmanzano@gmail.com>
2022-06-10 09:08:51 +0200

    test

--- blockchain.py
+++ blockchain.py
@@ -31,5 +31,37 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
     
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            # En la versión final añadiremos una variable que aleatoriamente elija la longitud de caracteres a buscar y otro que elija aleatoriamente los caracteres a buscar
+            # Utilizar polinomios más complicados para dificultar el minado
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2),encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
 
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            current_block = chain[block_index]
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            current_proof = current_block['proof']
+            hash_operation = hashlib.sha256(str(current_proof**2 - previous_proof**2),encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = current_block
+            block_index += 1
+        return True
+    
 # Parte 2 - Minado de un bloque de la cadena
